e0d6ab403d121136e32f7a43f0a8dce766406ed0
Cienszki <wilq.wdz@gmail.com>
2018-10-10 18:34:09 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,19 @@
+function drawTree(height) {
+    for ( var i = 0; i < height ; i++ ) {
+        var star = '*';
+        var space = ' ';        
+
+        for ( var j = 1; j <= i; j++ ) {
+            star = star + '**';            
+        }
+        var spacesBefore = space.repeat(height - i - 1);
+        star = spacesBefore + star;
+        console.log(star);
+    }
+}
+
+var levels = prompt('How many levels high should be the tree?');
+
+drawTree(levels);
+alert('Check console')
+
